ba5e1ca25bee4ca55e85f460ada38717a149ac0c
Hélcio Franco <helciofranco@me.com>
2024-03-21 10:26:42 -0400

    fix: adjust dir

--- scripts/unpublish.js
+++ scripts/unpublish.js
@@ -11,9 +11,11 @@ const DELETE_PACKAGES = process.env.DELETE_PACKAGES === 'true';
 const dryRun = DELETE_PACKAGES ? '' : '--dry-run';
 
 async function getPublicPackages() {
-  const packages = await readdir(join(__dirname, '../packages'), {
+  const base = join(__dirname, '../packages');
+  const packages = await readdir(base, {
     withFileTypes: true,
   });
+
   const packagesNames = await Promise.all(
     packages.map(async (p) => {
       try {
